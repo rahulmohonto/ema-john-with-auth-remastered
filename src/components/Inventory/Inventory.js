9df8e656b68c61ef5d3e4e9310c5eaf1925f97e2
rahulmohonto <rahulmohonto1999@gmail.com>
2021-04-01 18:59:10 +0600

    code updated

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+
+
+
+const Inventory = () => {
+    const handleAddProduct = () => {
+        fetch('http://localhost:4200/addProduct', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(fakeData)
+        })
+    }
+    return (
+        <div>
+            <h1>Inventory coming soon...</h1>
+            <button onClick={handleAddProduct}>Add Product</button>
+        </div>
+    );
+};
+
+export default Inventory;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAGv74hnXoIWae1gYGLqfcieQ8f0FGQIw",
+  authDomain: "ema-john-with-auth-remastered.firebaseapp.com",
+  projectId: "ema-john-with-auth-remastered",
+  storageBucket: "ema-john-with-auth-remastered.appspot.com",
+  messagingSenderId: "992576403695",
+  appId: "1:992576403695:web:1283aaaa825efa6258831a"
+};
+export default firebaseConfig;
